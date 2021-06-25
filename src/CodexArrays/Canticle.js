418746f4bb509cc3428c56989a36d6f37366ee05
Ryan Krenzke <ryan.krenzke@gmail.com>
2021-06-25 14:36:15 -0400

    canticles progress

---
+++ src/CodexArrays/Canticle.js
@@ -0,0 +1,28 @@
+const Canticles = [
+    {
+        name: "Shroudpsalm",
+        rule: ["Unit gains benefit of Light Cover"]
+    },
+    {
+        name: "Chant of the Remorseless Fist",
+        rule: ["+1 to Strength characteristic for melee attacks"]
+    },
+    {
+        name: "Incantation of the Iron Soul",
+        rule: ["Ignore all modifiers when taking a Combat Attrition test", "5+ FNP"]
+    },
+    {
+        name: "Benediction of the Omnissiah",
+        rule: ["Each unit can re-roll one hit, one wound, and one damage roll during shooting phase"]
+    },
+    {
+        name: "Invocation of Machine Vengeance",
+        rule: "Roll addition D6 on Advance rolls. Discard the lowest"
+    },
+    {
+        name: "Litany of the Electromancer",
+        rule: ["-1 from enemy hit rolls for melee attacks"]
+    }
+]
+
+export default Canticles;
